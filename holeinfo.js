cd406954f18fb0dcaa11c5ff7c44ee90efbaa273
Adam <zealan@gmail.com>
2018-05-25 19:16:06 -0400

    set up holeinfo.js and fix html issue

--- holeinfo.js
+++ holeinfo.js
@@ -1,6 +1,126 @@
 var holeInfo = [
+	/* 1 */
 	{
-		info: "Whee!",
+		info: "<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 2 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 3 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 4 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 5 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 6 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 7 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 8 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 9 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 10 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 11 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 12 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 13 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 14 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 15 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 16 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 17 */
+	{
+		info: "<p>Info coming soon.</p>",
+		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
+		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
+	},
+	/* 18 */
+	{
+		info: "<p>Info coming soon.</p>",
 		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
 		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
 		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
