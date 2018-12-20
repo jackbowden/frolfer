a650ebcfdc6aca2dd5b1a30abe4bd0f7cea4ab05
Adam <zealan@gmail.com>
2018-12-19 19:44:37 -0500

    rework holeinfo.js to have less hardcoded data

--- holeinfo.js
+++ holeinfo.js
@@ -1,128 +1,20 @@
 var holeInfo = [
-	/* 1 */
-	{
-		info: "<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 2 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 3 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 4 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 5 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 6 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 7 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 8 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 9 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 10 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 11 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 12 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 13 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 14 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 15 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 16 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 17 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	},
-	/* 18 */
-	{
-		info: "<p>Info coming soon.</p>",
-		startImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		endImg: "https://scdbwiki.swem.wm.edu/wiki/images/b/b6/Oliver_sculpture.jpg",
-		map: "https://www.google.com/maps/d/u/1/embed?mid=1sF2h2uMQ23XYcj7APuWTyjHhxIA"
-	}
+	"<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
+	"2!",
+	"3!",
+	"4!",
+	"5!",
+	"6?",
+	"7",
+	"8",
+	"9",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
+	"15",
+	"16",
+	"17",
+	"18"
 ];
