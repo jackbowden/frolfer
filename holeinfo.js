3f1644a1b81a182fc4223c4e5534c72d27f4194f
Adam <zealan@gmail.com>
2018-12-19 19:50:17 -0500

    rework holeinfo.js again to include target and watchFor

--- holeinfo.js
+++ holeinfo.js
@@ -1,20 +1,127 @@
 var holeInfo = [
-	"<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
-	"2!",
-	"3!",
-	"4!",
-	"5!",
-	"6?",
-	"7",
-	"8",
-	"9",
-	"10",
-	"11",
-	"12",
-	"13",
-	"14",
-	"15",
-	"16",
-	"17",
-	"18"
+	/* 1 */
+	{
+		info: "<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 2 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 3 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 4 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 5 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 6 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 7 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 8 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 9 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 10 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 11 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 12 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 13 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 14 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 15 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 16 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 17 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	},
+
+	/* 18 */
+	{
+		info: "",
+		target: "",
+		watchFor: ""
+	}
 ];
