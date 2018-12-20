96e2de074cdce8b22f87542252582411978b7a84
Adam <zealan@gmail.com>
2018-12-19 20:20:09 -0500

    fill out holeinfo.js

--- holeinfo.js
+++ holeinfo.js
@@ -2,126 +2,126 @@ var holeInfo = [
 	/* 1 */
 	{
 		info: "<p>Skip the disc straight down the road into the emergency beacon. <em>Obviously, do not activate the beacon.</em></p><p>Stop if cars are passing.</p><p>Move your disc if a car may threaten to crush it.</p><p>Hole-in-one has been achieved (2016).</p>",
-		target: "",
-		watchFor: ""
+		target: "Emergency beacon",
+		watchFor: "Cars, people, gutters, bushes"
 	},
 
 	/* 2 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Huck the disc over the terrace, then toss into the light post.</p> <br> <p>Avoid throwing into prickly bushes near bicycle posts.</p><p>Watch for people on the terrace and exiting Lodge 1.</p>",
+		target: "Light post",
+		watchFor: "Cars, people, bushes"
 	},
 
 	/* 3 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip the disc down the road and toss into metallic sign.</p> <br> <p>Avoid throwing into gutters and the Crim Dell.</p><p>Watch for people coming up the road.</p><p>Don't miss the throw to target -- disc may go deep into woods.</p>",
+		target: "Metallic sign",
+		watchFor: "Gutters, the Crim Dell, woods, people, bushes"
 	},
 
 	/* 4 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Throw disc across the road, up the green hill or stairs. Toss into light post next to sidewalk.</p> <br> <p>Avoid throwing into prickly bushes.</p> <p>Watch for people walking on the sidewalk.</p>",
+		target: "Light post",
+		watchFor: "Cars, people, bushes"
 	},
 
 	/* 5 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Throw the disc up or around the large tree, then sip down sidewalk into sundial.</p><br><p>Avoid throwing into prickly bushes.</p><p>Watch for people walking on the sidewalks and entering or exiting Swem Library.</p>",
+		target: "Sundial",
+		watchFor: "People, bushes"
 	},
 
 	/* 6 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip disc down brick path, then throw into metallic artwork.</p><br><p>Watch for people walking on the sidewalks</p>",
+		target: "Metallic artwork",
+		watchFor: "People"
 	},
 
 	/* 7 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Roll the disc across Barksdale Field, then toss into sidewalk lamp.</p><br><p>Watch for people walking on the sidewalks and exiting the dormitories.</p>",
+		target: "Sidewalk lamp in between Hardy & Lemon Halls",
+		watchFor: "People"
 	},
 
 	/* 8 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Flick the disc around the corner of Hardy Hall, skip down the brick path, then carefully toss across the street into the front door of Chandler Hall.</p><br><p>Go through Chandler directly to the opposite side to start the next hole.</p><br><p>Watch closely for moving cars on Landrum Drive.</p><p>Avoid hitting parked cars along Landrum Drive in front of Chandler's front door.</p><p>Watch for people walking on the sidewalks and exiting the dormitory.</p>",
+		target: "Southern front door of Chandler Hall",
+		watchFor: "Moving cars, parked cars, and people"
 	},
 
 	/* 9 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Shoot the disc across the gravel sidewalk and up over the brick wall for the most direct path. Toss into the emergency beacon.</p><br><p>Watch for people walking on the sidewalks.</p>",
+		target: "Emergency beacon adjacent to McGlothlin-Street Hall",
+		watchFor: "People, bushes"
 	},
 
 	/* 10 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip disc down brick pathway in front of McGlothlin-Street Hall, then throw towards and into Thomas Jefferson statue.</p><br><p>Watch for people walking on the sidewalks and exiting McGlothlin-Street Hall.</p>",
+		target: "Thomas Jefferson statue",
+		watchFor: "People"
 	},
 
 	/* 11 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Shoot the disc over the brick ledge flanking Thomas Jefferson statue into the field beyond -- watch carefully where you throw the disc! Toss towards and into lamp.</p><br><p>Watch for people walking on the sidewalks.</p>",
+		target: "Lamp adjacent to Barrett Hall",
+		watchFor: "People, large trees"
 	},
 
 	/* 12 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip the disc down the brick path lateral to Jefferson Hall, and toss into the lamp immediately ahead. </p><br><p>Watch for people walking on the sidewalks and exiting Jefferson Hall.</p>",
+		target: "Lamp adjacent to Ewell Circle",
+		watchFor: "People"
 	},
 
 	/* 13 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip the disc down the brick path lateral to Ewell Hall, and toss into bulletin board.</p><br><p>Watch for people walking on the sidewalks and exiting Ewell Hall.</p>",
+		target: "Bulletin board adjacent to Ewell Hall",
+		watchFor: "People"
 	},
 
 	/* 14 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Skip the disc down the brick path perpendicular to the Sunken Gardens, then toss into James Monroe statue.</p><br><p>Watch for people walking on the sidewalks and exiting Tucker Hall.</p>",
+		target: "James Monroe statue",
+		watchFor: "People"
 	},
 
 	/* 15 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Flick the disc around the side of Tucker Hall, across James Blair Dr. and into the western side door of Monroe Hall. </p><br><p>Watch for moving cars along James Blair Dr.</p><p>Watch for people walking on the sidewalks and exiting Monroe Hall.</p>",
+		target: "Western side door of Monroe Hall ",
+		watchFor: "People, moving cars"
 	},
 
 	/* 16 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Flick the disc around the side Monroe Hall and toss into the southern front door of Monroe Hall.</p><br><p>Watch for people walking on the sidewalks and exiting Monroe Hall.</p>",
+		target: "Southern front door of Monroe Hall",
+		watchFor: "People"
 	},
 
 	/* 17 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Throw disc towards Tyler Hall, avoiding parked cars and moving cars along James Blair Dr. Toss into James Blair statue. </p><br><p>Watch for parked and moving cars on James Blair Dr. </p><p>Watch for people walking on the sidewalks.</p>",
+		target: "James Blair statue",
+		watchFor: "People, moving cars, parked cars"
 	},
 
 	/* 18 */
 	{
-		info: "",
-		target: "",
-		watchFor: ""
+		info: "<p>Bonus hole: Stand on the podium of the James Blair Statue, and throw a hammer to the Blow Memorial Hall sign. One throw only!</p><br><p>Watch for moving cars on James Blair Dr. </p>",
+		target: "Blow Memorial Hall sign",
+		watchFor: "People, moving cars"
 	}
 ];
